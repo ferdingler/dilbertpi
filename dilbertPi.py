45dbc607c79fdf6d27aac9416167861e93ab0c21
Fer Dingler <ferdingler@gmail.com>
2014-04-19 20:23:03 -0700

    Done

--- dilbertPi.py
+++ dilbertPi.py
@@ -35,13 +35,9 @@ req = urllib2.Request(dilberthost + 'endpoint/create', data, headers)
 responseGuid = urllib2.urlopen(req)
 
 #=============================
-# ASKING FOR INTERVAL
+# ASKING FOR INTERVAL & ENDPOINT
 #=============================
-interval = float(raw_input("\033[94mHow frequent (in seconds) do you want me to sense?:\033[0m "))
-
-#=============================
-# ASKING FOR ENDPOINT
-#=============================
+interval = float(raw_input("\033[94mHow frequent (in seconds) do you want Dilbert to sense?:\033[0m "))
 endpointUrl = raw_input("\033[94mType endpoint URL where Dilbert should POST the data:\033[0m ")
 
 #=============================
@@ -55,23 +51,25 @@ while True:
 	# GETTING FAKE DATA FROM DILBERT
 	fakeDataResponse = urllib2.urlopen(dilbertRequest)
 	fakeData = fakeDataResponse.read()
-	fakeDataJson = json.loads(fakeData)
+
+	# FEEDBACK IN TERMINAL
+	if count % 2 == 0:
+		print "\033[92m"
+	else:
+		print "\033[93m"
+	print fakeData
+	print "\033[0m"
 
 	# POSTING THE FAKE DATA TO THE USER-DEFINED ENDPOINT
-	data = urllib.urlencode(fakeDataJson)
-	headers = { 'Content-type' : 'application/json' }
-	endpointRequest = urllib2.Request(endpointUrl, data, headers)
-	urllib2.urlopen(endpointRequest)
-	
-	# data = urllib.urlencode(fakeDataJson)
-	# headers = { 'Content-type' : 'application/json' }
-	# endpointRequest = urllib2.Request(endpointUrl, data, headers)
-	# try: 
-	# 	urllib2.urlopen(endpointRequest)
-	# except urllib2.HTTPError, e:
-	# 	print "HTTPError = " + str(e.code)
-	# except urllib2.URLError, e:
-	#     print "URLError = " + str(e.reason)
+	#data = urllib.urlencode(fakeData)
+	endpointRequest = urllib2.Request(endpointUrl, fakeData, { 'Content-type' : 'application/json' })
+
+	try: 
+		urllib2.urlopen(endpointRequest)
+	except urllib2.HTTPError, e:
+		print "HTTPError = " + str(e.code)
+	except urllib2.URLError, e:
+	    print "URLError = " + str(e.reason)
 
 	sleep(interval)
 	count = count + 1
